d4b60e39a002c9f1e18f5c9e8a70891f4de5ee87
LoneE-PP <74532864+LoneE-PP@users.noreply.github.com>
2023-04-25 09:49:02 +0200

    tailwindexport

---
+++ Tailwindexport/tailwind-variables.js
@@ -0,0 +1,43 @@
+module.exports = {
+    'colors': {
+        'black-#000000': '#000000', 
+        'white-#ffffff': '#ffffff', 
+        'grey': {
+            '900-#191919': '#191919', 
+            '700-#333333': '#333333', 
+            '500-#7f7f7f': '#7f7f7f', 
+            '300-#cccccc': '#cccccc', 
+            '100-#ebebeb': '#ebebeb', 
+        },
+        'green': {
+            'earth-#354c39': '#354c39', 
+            'primary-#cfe548': '#cfe548', 
+            'tint-#f0ff99': '#f0ff99', 
+            'pastell-#f4fce': '#f4f4ce', 
+            'neutral-#ccccco': '#ccccc0', 
+        },
+        'purple': {
+            'earth-#3a364c': '#3a364c', 
+            'primary-#7546e8': '#7546e8', 
+            'tint-#9e99ff': '#9e99ff', 
+            'pastell-#cecef2': '#cecef2', 
+            'neutral-#c2c2cc': '#c2c2cc', 
+        },
+        'blue': {
+            'earth-#354349': '#354349', 
+            'primary-#48b1e5': '#48b1e5', 
+            'tint-#99e2ff': '#99e2ff', 
+            'pastell-#cee6ef': '#cee6ef', 
+            'neutral-#c4cacc': '#c4cacc', 
+        },
+        'orange': {
+            'earth-#4c4636': '#4c4636', 
+            'primary-#e08e4b': '#e08e4b', 
+            'tint-#ffc799': '#ffc799', 
+            'pastell-#eddcce': '#eddcce', 
+            'neutral-#cccac8': '#cccac8', 
+        },
+    },
+    'boxShadow': {
+    }
+}
